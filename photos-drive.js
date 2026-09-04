e9a4a621bde2f713bef8d1c8534b7549c49b0b4d
Du Jie <99374085@qq.com>
2026-09-03 20:06:48 -0700

    Point review photos at GitHub Pages URLs

--- photos-drive.js
+++ photos-drive.js
@@ -1,9 +1,10 @@
-const DRIVE_FOLDER = "https://drive.google.com/drive/folders/1fHmntNUhxfQd-5sVNaKrN509X58SDJvj";
-const DRIVE_PHOTOS = {
-  "photos/p135-central-park.jpg": "https://drive.google.com/thumbnail?id=1fec29vvX2ww-h8jRaa7ALecXdo5mjcLt&sz=w1200",
-  "photos/email-jerry-bill.jpg": "https://drive.google.com/thumbnail?id=1HOhwYmXWTe_DF3tQXqudH6b0BCJ7t4xv&sz=w1200",
-  "photos/p141-holidays.jpg": "https://drive.google.com/thumbnail?id=1aUsh09hRf7ROXa7G9lFOQ-U9pGuPOtwn&sz=w1200",
-  "photos/email-gavin-hospital.jpg": "https://drive.google.com/thumbnail?id=1W4aXkKySPXrJ9reuidz_BhGwpyuu0FPM&sz=w1200",
-  "photos/p147-dolphin.jpg": "https://drive.google.com/thumbnail?id=1Kcl-9Kjx_Iru6RBaOe_YJxhSSs3ctOQH&sz=w1200",
-  "photos/email-jessie-job.jpg": "https://drive.google.com/thumbnail?id=1vzGyrLFX9yLo9Vundsz91yqPBtFR7pJG&sz=w1200"
+const PHOTO_BASE = "https://adoocoke.github.io/pet-review/";
+const GH_PHOTOS = {
+  "photos/p135-central-park.jpg": PHOTO_BASE + "photos/p135-central-park.jpg",
+  "photos/email-jerry-bill.jpg": PHOTO_BASE + "photos/email-jerry-bill.jpg",
+  "photos/p141-holidays.jpg": PHOTO_BASE + "photos/p141-holidays.jpg",
+  "photos/email-gavin-hospital.jpg": PHOTO_BASE + "photos/email-gavin-hospital.jpg",
+  "photos/p147-dolphin.jpg": PHOTO_BASE + "photos/p147-dolphin.jpg",
+  "photos/email-jessie-job.jpg": PHOTO_BASE + "photos/email-jessie-job.jpg"
 };
+const DRIVE_PHOTOS = GH_PHOTOS;
