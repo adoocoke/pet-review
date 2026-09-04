b1f0450f6f7576eef26f846024943f00c14941cf
Du Jie <99374085@qq.com>
2026-09-03 19:55:42 -0700

    Map review photos to Google Drive file IDs

---
+++ photos-drive.js
@@ -0,0 +1,9 @@
+const DRIVE_FOLDER = "https://drive.google.com/drive/folders/1fHmntNUhxfQd-5sVNaKrN509X58SDJvj";
+const DRIVE_PHOTOS = {
+  "photos/p135-central-park.jpg": "https://drive.google.com/thumbnail?id=1fec29vvX2ww-h8jRaa7ALecXdo5mjcLt&sz=w1200",
+  "photos/email-jerry-bill.jpg": "https://drive.google.com/thumbnail?id=1HOhwYmXWTe_DF3tQXqudH6b0BCJ7t4xv&sz=w1200",
+  "photos/p141-holidays.jpg": "https://drive.google.com/thumbnail?id=1aUsh09hRf7ROXa7G9lFOQ-U9pGuPOtwn&sz=w1200",
+  "photos/email-gavin-hospital.jpg": "https://drive.google.com/thumbnail?id=1W4aXkKySPXrJ9reuidz_BhGwpyuu0FPM&sz=w1200",
+  "photos/p147-dolphin.jpg": "https://drive.google.com/thumbnail?id=1Kcl-9Kjx_Iru6RBaOe_YJxhSSs3ctOQH&sz=w1200",
+  "photos/email-jessie-job.jpg": "https://drive.google.com/thumbnail?id=1vzGyrLFX9yLo9Vundsz91yqPBtFR7pJG&sz=w1200"
+};
