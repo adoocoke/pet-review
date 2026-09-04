6b6e79062d68f15a0ceb76e46e9b946b908d464a
Du Jie <99374085@qq.com>
2026-09-03 21:54:36 -0700

    Stop using 404 GitHub photo URLs; use embedded PHOTO first

--- photos-drive.js
+++ photos-drive.js
@@ -1,10 +1,2 @@
-const PHOTO_BASE = "https://adoocoke.github.io/pet-review/";
-const GH_PHOTOS = {
-  "photos/p135-central-park.jpg": PHOTO_BASE + "photos/p135-central-park.jpg",
-  "photos/email-jerry-bill.jpg": PHOTO_BASE + "photos/email-jerry-bill.jpg",
-  "photos/p141-holidays.jpg": PHOTO_BASE + "photos/p141-holidays.jpg",
-  "photos/email-gavin-hospital.jpg": PHOTO_BASE + "photos/email-gavin-hospital.jpg",
-  "photos/p147-dolphin.jpg": PHOTO_BASE + "photos/p147-dolphin.jpg",
-  "photos/email-jessie-job.jpg": PHOTO_BASE + "photos/email-jessie-job.jpg"
-};
-const DRIVE_PHOTOS = GH_PHOTOS;
+window.PHOTO = window.PHOTO || {};
+const DRIVE_PHOTOS = {};
