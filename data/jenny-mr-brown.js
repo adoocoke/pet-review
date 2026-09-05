ea1f0e0a1ff210d79bd03f6bcd81a2f7d649dcfa
Du Jie <99374085@qq.com>
2026-09-04 19:33:49 -0700

    content: Sophia / Jenny / Justin 邮件错题 #23

---
+++ data/jenny-mr-brown.js
@@ -0,0 +1,18 @@
+ITEMS.push(
+{id:"jenny-q2",title:"Jenny 请假 第 2 题 back",meta:"KET Part 5 · Test 2 · p.96",
+prompt:"I hope she will be <span class=\"blank\">2</span> at school this Wednesday.",
+options:[{key:"able",text:"able"},{key:"back",text:"back"},{key:"good",text:"good"}],
+answer:"back",fill:"back",
+ok:"be back at school = 星期三能回学校。",
+bad:"书上写了 able。be able 后面要接 to do，不能 be able at school。",
+note:"<p>Jenny 今天胃痛去医院。I hope she will be <b>2</b> at school this Wednesday.</p><p><b>错因</b>：想说能够来上学，选了 able。</p><p><b>对</b>：<b>be back at school</b> = 回到学校。红笔也改成了 back。</p><p>be able to come to school 才对；空格后是 at school，只能 back。</p>",
+photo:"photos/p96-jenny-amy.jpg"},
+{id:"jenny-q5",title:"Jenny 请假 第 5 题 is",meta:"KET Part 5 · Test 2 · p.96",
+prompt:"I am sorry Jenny <span class=\"blank\">5</span> ill.",
+options:[{key:"was",text:"was"},{key:"is",text:"is"},{key:"be",text:"be"}],
+answer:"is",fill:"is",
+ok:"Jenny is ill = 她现在还病着。",
+bad:"书上写了 was。老师回信时她还没好，用现在时 is。",
+note:"<p>爸爸说今天不能来。老师回：I am sorry Jenny <b>5</b> ill. Amanda 今天下午会把作业送过去。</p><p><b>错因</b>：觉得生病是已经发生的，用了 was。</p><p><b>对</b>：作业还要送回家，说明人还病着 → <b>is</b> ill。</p>",
+photo:"photos/p96-jenny-amy.jpg"}
+);
---
+++ data/justin-tommy.js
@@ -0,0 +1,10 @@
+ITEMS.push(
+{id:"justin-q27",title:"Justin 邮件 第 27 题 of",meta:"KET Part 5 · p.124",
+prompt:"weren't you afraid <span class=\"blank\">27</span> those tigers?",
+options:[{key:"about",text:"about"},{key:"of",text:"of"},{key:"with",text:"with"}],
+answer:"of",fill:"of",
+ok:"afraid of those tigers = 怕那些老虎。",
+bad:"书上写了 about。afraid 后面固定接 of。",
+note:"<p>weren't you afraid <b>27</b> those tigers? They look so scary!</p><p><b>afraid of somebody/something</b>。</p><p>about 用在 worry about / talk about，不是 afraid。</p>",
+photo:"photos/p124-justin-tommy.jpg"}
+);
