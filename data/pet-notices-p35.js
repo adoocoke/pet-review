59bc1186ddc91502d3ee51cbda1ae5e884d741c2
Du Jie <99374085@qq.com>
2026-09-07 04:40:02 -0700

    content: PET notices p.35/37/38/40 #24

---
+++ data/pet-notices-p35.js
@@ -0,0 +1,15 @@
+ITEMS.push(
+{id:"pet35-q3",exam:"PET",kind:"notice",title:"泳池告示 第 3 题",meta:"PET 告示理解 · Exercise 1 · p.35",
+notice:"Dear Members,\nKindly be informed that the Swimming Pool will be closed for Junior Swimming Contest this Saturday. We sincerely apologise for any inconvenience caused.",
+prompt:"这张告示在说什么？",
+options:[
+  {key:"A",text:"A The swimming club is going to hold contest this Saturday."},
+  {key:"B",text:"B Club members can use the swimming pool this Saturday."},
+  {key:"C",text:"C The swimming pool is reserved for competition this Saturday."}
+],
+answer:"C",fill:"reserved for competition",
+ok:"泳池周六关闭，是留给少年游泳比赛用的。",
+bad:"书上 A 和 C 都圈了。A 只说到「有比赛」，没说泳池本身不能用。",
+note:"<p>告示：Swimming Pool <b>will be closed for Junior Swimming Contest this Saturday</b>。</p><p><b>错因</b>：看见 Contest 就选 A。A 说的是 club 要办比赛，告示真正要会员知道的是：<b>泳池周六不开放，留给比赛</b>。</p><p><b>对</b>：C reserved for competition = 场地被比赛占用。</p><p>B 正好相反：会员这天不能用。</p><p>PET 告示题：先找告示要你<b>做什么 / 不能做什么</b>，不要只抓一个单词。</p>",
+photo:"photos/p35-notices.jpg"}
+);
---
+++ data/pet-notices-p40.js
@@ -0,0 +1,15 @@
+ITEMS.push(
+{id:"pet40-q4",exam:"PET",kind:"notice",title:"二手自行车 第 4 题",meta:"PET 告示理解 · p.40",
+notice:"Second-hand bicycle with reasonable price.\nWell kept by previous owners.\nFor more information, please call Martin by 555-666-777.",
+prompt:"Martin wants people to call him because",
+options:[
+  {key:"A",text:"A he would like to help them buy new bicycles."},
+  {key:"B",text:"B he wants to share his experiences of taking care of bicycles."},
+  {key:"C",text:"C he can help them to learn more about the used bicycle on sale."}
+],
+answer:"C",fill:"used bicycle on sale",
+ok:"打电话是为了了解这辆在卖的二手车。",
+bad:"书上 B、C 都圈了。Well kept 只是说车保养得好，不是要分享养车经验。",
+note:"<p>告示：二手车在卖，价格合理，保养得好。<b>For more information, please call Martin.</b></p><p><b>错因</b>：看见 Well kept / previous owners，以为 Martin 要分享养车经验。</p><p><b>对</b>：call 的目的是 more information about <b>the used bicycle on sale</b>。</p><p>A 说帮人买新车，告示是卖旧车。</p><p>second-hand = used = 二手。</p>",
+photo:"photos/p40-notices.jpg"}
+);
