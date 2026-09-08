a72a18921fa128b36965eae17a1de543454dded4
Du Jie <99374085@qq.com>
2026-09-07 20:36:26 -0700

    v0.18.0 add full-passage templates for PET gap fill

---
+++ data/pet-passages.js
@@ -0,0 +1,38 @@
+window.GAP_PASSAGES = {
+jour: {
+  title: "Teenager Journalist",
+  given: { "2": "like", "4": "with" },
+  html:
+'<p>Journalists can play an important part in our society, telling people what is happening in the world and helping them to understand events. Being young doesn\'t mean you don\'t know interesting things <span class="blank" data-n="1"></span> have opinions that count.</p>' +
+'<p>Some people are naturally curious; they like to get to the bottom of things. Does that sound <span class="blank" data-n="2"></span> you? Then you could be a journalist. By starting to write <span class="blank" data-n="3"></span> you are young, your writing skills will develop, along <span class="blank" data-n="4"></span> your ability to interview people. If you want to begin writing, you could start a newspaper with your friends. <span class="blank" data-n="5"></span> one of you could cover a different topic or area that you think is interesting. Or you could enter this year\'s competition of journalists, with an article that is up to 500 words long.</p>' +
+'<p>Remember, although a strong argument is important, so <span class="blank" data-n="6"></span> facts. Some of what you write may be opinion but facts can only ever be correct or incorrect.</p>'
+},
+gard: {
+  title: "Grow a School Garden",
+  given: { "1": "if", "2": "up", "4": "them", "5": "to" },
+  html:
+'<p>Not every school has its own garden, but if your school does, you and your classmates can help.</p>' +
+'<p>Start by asking your teacher <span class="blank" data-n="1"></span> you can do some work to improve the school garden. Most teachers will be keen to have some help with this. They might let you set <span class="blank" data-n="2"></span> after-school club for gardeners, allow you to work on it at break times <span class="blank" data-n="3"></span> make it part of a lesson where the whole class can get involved.</p>' +
+'<p>You can learn about identifying weeds, and then help pull <span class="blank" data-n="4"></span> up and dig out their roots. You can learn how <span class="blank" data-n="5"></span> sow seeds. You can also learn planting all sorts of vegetables and herbs. You\'ll start to recognize the different parts of flowers and insects. Hopefully you\'ll see an increase <span class="blank" data-n="6"></span> bees, butterflies and other insects.</p>' +
+'<p>With care and attention, you could make your school a little bit greener.</p>'
+},
+nba: {
+  title: "My NBA Experience",
+  given: { "3": "when" },
+  html:
+'<p>Finding out I was picked to play for the Europe and Middle East team was unbelievable. At first <span class="blank" data-n="1"></span> didn\'t sink in, but when my family, friends and teachers started to say they are happy for me it sank in and I was overjoyed.</p>' +
+'<p>The experience was amazing. I enjoyed going to Disney World and the food. My favourite part of the whole experience <span class="blank" data-n="2"></span> playing against the other teams.</p>' +
+'<p>I definitely made friends for life. We all have a group chat from <span class="blank" data-n="3"></span> we first went to Bologna, and we use it all the time to keep <span class="blank" data-n="4"></span> touch. The week before in Valencia really helped as we got to meet the coaches and had time to get <span class="blank" data-n="5"></span> each other.</p>' +
+'<p>Coming back to London was hard as I missed my teammates and the whole experience, <span class="blank" data-n="6"></span> a few days home with my mum\'s cooking helped.</p>'
+},
+live: {
+  title: "Things We Can\'t Live Without",
+  given: { "1": "as", "4": "or" },
+  html:
+'<p>What are the most important things in your life\u2014the things you just couldn\'t live without?</p>' +
+'<p>Some of the things that Brits simply cannot do without include a television, a fast Internet connection and an iPhone, as well <span class="blank" data-n="1"></span> essentials such as a daily shower, a <span class="blank" data-n="2"></span> of glasses and central heating.</p>' +
+'<p>Treats include a car, a foreign holiday, a full English breakfast, and a drink in the pub. Other drinks that featured in the list <span class="blank" data-n="3"></span> a glass of wine, a mug of tea and a cup of coffee <span class="blank" data-n="4"></span> hot chocolate.</p>' +
+'<p>Things <span class="blank" data-n="5"></span> satisfy our emotional needs also appeared on the list, including a hug, a good friend, a night on the sofa, a good cry, a solid marriage and an \'love you\' every now and then.</p>' +
+'<p>Interestingly, many people often ignore the essential things in life <span class="blank" data-n="6"></span> as love and friendship in favour of material things. Do any of you have things that you cannot live without?</p>'
+}
+};
